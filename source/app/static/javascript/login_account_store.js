45638890b772c44363dc1d5b2622ca9f5ccb139a
TechAbraao <abraao.cutielips@hotmail.com>
2025-09-19 09:56:47 -0300

    feat: adding news templates

--- source/app/static/javascript/login_account_store.js
+++ source/app/static/javascript/login_account_store.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
         let email = $("#email").val();
         let password = $("#password").val();
 
-       $.ajax({
+        $.ajax({
             url: loginURL,
             method: "POST",
             contentType: "application/json",
@@ -14,13 +14,49 @@ $(document).ready(function () {
                 password: password,
             }),
             success: function (response) {
-                console.log("Response: ", response)
+                let successMessage = response.message || "Login successful";
+
+                $("aside .alert_base_login_success .alert_login_store_success").text(successMessage);
+                $("aside .alert_base_login_success").removeClass("hidden");
+
+                setTimeout(() => {
+                    $("aside .alert_base_login_success").addClass("hidden");
+                }, 2300);
+
+                setTimeout(function () {
+                    window.location.href = "/dashboard";
+                }, 2300)
             },
             error: function (xhr) {
-                 let response = JSON.parse(xhr.responseText);
-                 console.error("Error(s): ", response)
+                let errorMessage = "";
+                try {
+                    let response = JSON.parse(xhr.responseText);
+
+                    if (response.errors) {
+                        if (typeof response.errors === "string") {
+                            errorMessage = response.errors;
+                        } else {
+                            for (let field in response.errors) {
+                                errorMessage += `${response.errors[field].join(", ")}\n`;
+                            }
+                        }
+                    } else if (response.message) {
+                        errorMessage = response.message;
+                    } else {
+                        errorMessage = "An unknown error occurred.";
+                    }
+
+                } catch (e) {
+                    errorMessage = xhr.responseText;
+                }
+
+                $("aside .alert_base_login_error .alert_login_store_error").text(errorMessage);
+                $("aside .alert_base_login_error").removeClass("hidden");
+
+                setTimeout(() => {
+                    $("aside .alert_base_login_error").addClass("hidden");
+                }, 4000)
             }
         });
-
     })
 })
